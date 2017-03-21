0a5af60b9f819fa4072c803f4bc6a992dab7fbc7
alumno <alumno>
2017-03-20 21:57:56 -0300

    Ejercicio 6 de la guia

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
-echo "Hola Mundo";
+echo "Hola Mundo<br>";
 //1.Constructor
 //$arrayCtr = array(rand(0,10),rand(0,10),rand(0,10)rand(0,10)rand(0,10));
 $arrayCtr = array(1,3,5,7,9);
 
 //2.[0]
+$sumaIndices=0;
 $arrayFor =array();
 for ($i=0; $i < 5; $i++) { 
-    $arrayFor[$i]=rand(0,100);
+    $arrayFor[$i]=rand(0,10);
+    $sumaIndices=$sumaIndices + $arrayFor[$i];        
 }
+$promedio=$sumaIndices/5;
+if ($promedio <6) {
+    echo "Es menor<br>";
+} else {
+    if ($promedio>6) {
+        echo "Es mayor<br>";        
+    }
+    else
+    echo "Es igual";
+
+}
+
 
 //3.push()
 $arrayPush=array();
